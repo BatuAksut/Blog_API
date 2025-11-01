34a4e8d769d22aa1e76829e6d007dbd5d6b46684
ossan <ivan.pesenti.dev@gmail.com>
2025-11-01 10:50:40 +0100

    review 2025-11-01

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -7,78 +7,83 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+  // FIXME: the name of the endpoint address must be lowercase.
+  // TODO: have a look at this standard guidelines: https://opensource.zalando.com/restful-api-guidelines/#table-of-contents
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly UserManager<ApplicationUser> userManager;
+    private readonly ITokenRepository tokenRepository;
+
+    public AuthController(UserManager<ApplicationUser> userManager, ITokenRepository tokenRepository)
     {
-        private readonly UserManager<ApplicationUser> userManager;
-        private readonly ITokenRepository tokenRepository;
-
-        public AuthController(UserManager<ApplicationUser> userManager, ITokenRepository tokenRepository)
-        {
-            this.userManager = userManager;
-            this.tokenRepository = tokenRepository;
-        }
-
-
-
-        [HttpPost]
-        [Route("Register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
-        {
-            var applicationUser = new ApplicationUser()
-            {
-                UserName = registerDto.Username,
-                Email = registerDto.Username,
-                Firstname = registerDto.Firstname,
-                Lastname = registerDto.Lastname
-            };
-            var identityResult = await userManager.CreateAsync(applicationUser, registerDto.Password);
-            if (!identityResult.Succeeded)
-            {
-                
-                var errors = identityResult.Errors.Select(e => e.Description);
-                return BadRequest(new { message = "User creation failed", errors });
-            }
-
-            if (registerDto.Roles != null && registerDto.Roles.Any())
-            {
-                identityResult = await userManager.AddToRolesAsync(applicationUser, registerDto.Roles);
-                if (!identityResult.Succeeded)
-                {
-                    var errors = identityResult.Errors.Select(e => e.Description);
-                    return BadRequest(new { message = "Adding roles failed", errors });
-                }
-            }
-
-            return Ok("User registered successfully");
-        }
-
-
-
-        [HttpPost]
-        [Route("Login")]
-        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
-        {
-            var applicationUser = await userManager.FindByEmailAsync(loginDto.Username);
-            if (applicationUser != null && await userManager.CheckPasswordAsync(applicationUser, loginDto.Password))
-            {
-
-                var roles = await userManager.GetRolesAsync(applicationUser);
-                if (roles != null && roles.Count > 0)
-                {
-                    var jwtToken = tokenRepository.CreateJWTToken(applicationUser, roles.ToList());
-                    var response = new LoginResponseDto()
-                    {
-                        JwtToken = jwtToken
-                    };
-                    return Ok(response);
-                }
-
-
-
-            }
-            return BadRequest("Invalid username or password");
-        }
+      this.userManager = userManager;
+      this.tokenRepository = tokenRepository;
     }
+
+
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+    {
+      // FIXME: some validation logic? together with mapping logic.
+      var applicationUser = new ApplicationUser()
+      {
+        UserName = registerDto.Username,
+        Email = registerDto.Username,
+        Firstname = registerDto.Firstname,
+        Lastname = registerDto.Lastname
+      };
+      var identityResult = await userManager.CreateAsync(applicationUser, registerDto.Password);
+      if (!identityResult.Succeeded)
+      {
+
+        var errors = identityResult.Errors.Select(e => e.Description);
+        return BadRequest(new { message = "User creation failed", errors });
+      }
+
+      if (registerDto.Roles != null && registerDto.Roles.Any())
+      {
+        identityResult = await userManager.AddToRolesAsync(applicationUser, registerDto.Roles);
+        if (!identityResult.Succeeded)
+        {
+          var errors = identityResult.Errors.Select(e => e.Description);
+          return BadRequest(new { message = "Adding roles failed", errors });
+        }
+      }
+
+      // FIXME: this is a POST, it creates a record based on an entity. It must return 201 - Created together with the URI of the resource.
+      return Ok("User registered successfully");
+    }
+
+
+
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+      var applicationUser = await userManager.FindByEmailAsync(loginDto.Username);
+      if (applicationUser != null && await userManager.CheckPasswordAsync(applicationUser, loginDto.Password))
+      {
+
+        var roles = await userManager.GetRolesAsync(applicationUser);
+        // [Q]: we are not logging users without any roles? If this is the case, we're returning "Invalid username or password" which is misleading.
+        if (roles != null && roles.Count > 0)
+        {
+          var jwtToken = tokenRepository.CreateJWTToken(applicationUser, roles.ToList());
+          var response = new LoginResponseDto()
+          {
+            JwtToken = jwtToken
+          };
+          return Ok(response);
+        }
+
+
+
+      }
+      return BadRequest("Invalid username or password");
+    }
+  }
 }
--- DataAccess/Concrete/CommentRepository.cs
+++ DataAccess/Concrete/CommentRepository.cs
@@ -9,118 +9,124 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete
 {
-    public class CommentRepository : ICommentRepository
+  public class CommentRepository : ICommentRepository
+  {
+    private readonly AppDbContext context;
+
+    public CommentRepository(AppDbContext context)
     {
-        private readonly AppDbContext context;
+      this.context = context;
+    }
+    public async Task<Comment> CreateAsync(Comment comment)
+    {
+      var commentToAdd = new Comment
+      {
+        BlogPostId = comment.BlogPostId,
+        Content = comment.Content,
+        ApplicationUserId = comment.ApplicationUserId,
+        // FIXME: "CreatedAt" should be automatically set.
+        CreatedAt = comment.CreatedAt
+      };
 
-        public CommentRepository(AppDbContext context)
+      await context.Comments.AddAsync(commentToAdd);
+      await context.SaveChangesAsync();
+
+      // FIXME: warning
+      return await context.Comments
+          .Include(c => c.ApplicationUser)
+          .Include(c => c.BlogPost)
+          .FirstOrDefaultAsync(c => c.Id == commentToAdd.Id);
+    }
+
+    public async Task<Comment?> DeleteAsync(Guid id)
+    {
+      var commentToDelete = await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
+      if (commentToDelete == null) return null;
+
+      context.Comments.Remove(commentToDelete);
+      await context.SaveChangesAsync();
+      return commentToDelete;
+    }
+
+    public async Task<List<Comment>> GetAllAsync(
+string? filterOn = null,
+string? filterQuery = null,
+string? sortBy = null,
+bool isAscending = true,
+int pageNumber = 1,
+int pageSize = 20)
+    {
+      // Maksimum pageSize sınırı koyduk
+      pageSize = Math.Min(pageSize, 100);
+
+      var comments = context.Comments
+          .Include(x => x.ApplicationUser)
+          .Include(x => x.BlogPost)
+          .AsQueryable();
+
+      // Filtering
+      if (!string.IsNullOrWhiteSpace(filterOn) && !string.IsNullOrWhiteSpace(filterQuery))
+      {
+        switch (filterOn.ToLower())
         {
-            this.context = context;
+          case "content":
+            comments = comments.Where(x => x.Content.Contains(filterQuery));
+            break;
         }
-        public async Task<Comment> CreateAsync(Comment comment)
+      }
+
+      // Sorting
+      if (!string.IsNullOrWhiteSpace(sortBy))
+      {
+        comments = sortBy.ToLower() switch
         {
-            var commentToAdd = new Comment
-            {
-                BlogPostId = comment.BlogPostId,
-                Content = comment.Content,
-                ApplicationUserId = comment.ApplicationUserId,
-                CreatedAt=comment.CreatedAt
-            };
+          "content" => isAscending ? comments.OrderBy(x => x.Content) : comments.OrderByDescending(x => x.Content),
+          _ => comments
+        };
+      }
 
-            await context.Comments.AddAsync(commentToAdd);
-            await context.SaveChangesAsync();
+      // Pagination
+      var skipResults = (pageNumber - 1) * pageSize;
+      return await comments.Skip(skipResults).Take(pageSize).ToListAsync();
+    }
 
-            return await context.Comments
-                .Include(c => c.ApplicationUser)
-                .Include(c => c.BlogPost)
-                .FirstOrDefaultAsync(c => c.Id == commentToAdd.Id);
-        }
+    public async Task<Comment?> GetByIdAsync(Guid id)
+    {
+      // [Q]: Why you're always including User & BlogPost information?
+      // from a performance perspective is not ideal. We can save this discussion for the future BTW.
+      return await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
+    }
 
-        public async Task<Comment?> DeleteAsync(Guid id)
-        {
-            var commentToDelete = await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
-            if (commentToDelete == null) return null;
+    public async Task<Comment?> UpdateAsync(Guid id, Comment comment)
+    {
+      // [Q]: do you need to "Include" the other two entities?
+      var commentToUpdate = await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
 
-            context.Comments.Remove(commentToDelete);
-            await context.SaveChangesAsync();
-            return commentToDelete;
-        }
+      if (commentToUpdate == null) return null;
+      var userExists = await context.Users.AnyAsync(x => x.Id == comment.ApplicationUserId);
+      var blogExists = await context.BlogPosts.AnyAsync(x => x.Id == comment.BlogPostId);
 
-        public async Task<List<Comment>> GetAllAsync(
-    string? filterOn = null,
-    string? filterQuery = null,
-    string? sortBy = null,
-    bool isAscending = true,
-    int pageNumber = 1,
-    int pageSize = 20)
-        {
-            // Maksimum pageSize sınırı koyduk
-            pageSize = Math.Min(pageSize, 100);
+      // FIXME: make the single check to be detailed in the error message.
+      if (!userExists || !blogExists)
+        throw new Exception("User or BlogPost does not exist.");
 
-            var comments = context.Comments
-                .Include(x => x.ApplicationUser)
-                .Include(x => x.BlogPost)
-                .AsQueryable();
-
-            // Filtering
-            if (!string.IsNullOrWhiteSpace(filterOn) && !string.IsNullOrWhiteSpace(filterQuery))
-            {
-                switch (filterOn.ToLower())
-                {
-                    case "content":
-                        comments = comments.Where(x => x.Content.Contains(filterQuery));
-                        break;
-                }
-            }
-
-            // Sorting
-            if (!string.IsNullOrWhiteSpace(sortBy))
-            {
-                comments = sortBy.ToLower() switch
-                {
-                    "content" => isAscending ? comments.OrderBy(x => x.Content) : comments.OrderByDescending(x => x.Content),
-                    _ => comments
-                };
-            }
-
-            // Pagination
-            var skipResults = (pageNumber - 1) * pageSize;
-            return await comments.Skip(skipResults).Take(pageSize).ToListAsync();
-        }
-
-        public async Task<Comment?> GetByIdAsync(Guid id)
-        {
-            return await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<Comment?> UpdateAsync(Guid id, Comment comment)
-        {
-            var commentToUpdate = await context.Comments.Include(x => x.ApplicationUser).Include(x => x.BlogPost).FirstOrDefaultAsync(x => x.Id == id);
-
-            if (commentToUpdate == null) return null;
-            var userExists = await context.Users.AnyAsync(x => x.Id == comment.ApplicationUserId);
-            var blogExists = await context.BlogPosts.AnyAsync(x => x.Id == comment.BlogPostId);
-
-            if (!userExists || !blogExists)
-                throw new Exception("User or BlogPost does not exist.");
-
-            commentToUpdate.Content = comment.Content;
-            commentToUpdate.BlogPostId = comment.BlogPostId;
-            commentToUpdate.ApplicationUserId = comment.ApplicationUserId;
-            context.Comments.Update(commentToUpdate);
-            await context.SaveChangesAsync();
-            return commentToUpdate;
-
-        }
-
-        public async Task<List<Comment>> GetByBlogPostIdAsync(Guid blogPostId)
-        {
-            return await context.Comments
-                .Include(x => x.ApplicationUser)
-                .Include(x => x.BlogPost)
-                .Where(x => x.BlogPostId == blogPostId)
-                .ToListAsync();
-        }
+      commentToUpdate.Content = comment.Content;
+      commentToUpdate.BlogPostId = comment.BlogPostId;
+      commentToUpdate.ApplicationUserId = comment.ApplicationUserId;
+      context.Comments.Update(commentToUpdate);
+      await context.SaveChangesAsync();
+      return commentToUpdate;
 
     }
+
+    public async Task<List<Comment>> GetByBlogPostIdAsync(Guid blogPostId)
+    {
+      return await context.Comments
+          .Include(x => x.ApplicationUser)
+          .Include(x => x.BlogPost)
+          .Where(x => x.BlogPostId == blogPostId)
+          .ToListAsync();
+    }
+
+  }
 }
